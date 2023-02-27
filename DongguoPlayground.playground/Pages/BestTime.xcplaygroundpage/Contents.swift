78eaa6615f526d94a2b8e740112a920b5dc24a10
dongguowu <david.dong.guo@gmail.com>
2023-02-26 20:34:46 -0500

    best time

--- DongguoPlayground.playground/Pages/BestTime.xcplaygroundpage/Contents.swift
+++ DongguoPlayground.playground/Pages/BestTime.xcplaygroundpage/Contents.swift
@@ -1,7 +1,13 @@
 //: [Previous](@previous)
 
-import Foundation
+func maxProfit(_ prices: [Int]) -> Int {
+    var max = 0
+    return max
+}
 
 var greeting = "Hello, playground"
+
+var pricesA = [3,3,5,0,0,3,1,4]
+maxProfit(pricesA)
 
 //: [Next](@next)
