ec906c375fc9d039895dcea22cb5f473cfbfc2fd
dongguowu <david.dong.guo@gmail.com>
2023-02-28 22:15:15 -0500

    add closure

---
+++ DongguoPlayground.playground/Pages/AccessControl.xcplaygroundpage/Contents.swift
@@ -0,0 +1,97 @@
+//: # Access Control
+//: ## Access Levels
+//: - opne / public
+//: - internal (default)
+//: - fileprivate
+//: - private
+/**
+ * Mark methods and properties as private to prevent them from being accessed outside of the structure, class, or enumeration’s definition.
+ */
+
+// public structures can be accessed in other modules
+public struct User {
+    // internal is the default level of access control
+    let name: String
+    // fileprivate methods can only be accessed inside of the file they're declared in
+    mutating fileprivate func incrementVisitCount() {
+        visitCount += 1
+    }
+    // private properties can only be accessed inside their structure's definition
+    private var visitCount = 0
+}
+
+//: #3 Read-oly Computed Properties
+/**
+ * Read-only computed properties can be accessed, but not assigned to a new value.
+ * To define a read-only computed property, either use the get keyword without a set keyword,
+ * or omit keywords entirely.
+ */
+
+struct Room {
+    let width: Double
+    let height: Double
+    // omit keywords entirely
+    var squareFeet: Double {
+        return width * height
+    }
+    // only use GET
+    var description: String {
+        get {
+            return "This room is \(width) x \(height)"
+        }
+    }
+}
+
+//: ## Property Observers
+/**
+ * Property observers execute code whenever a property is changed.
+ * The willSet property observer is triggered right **before** the property is changed
+ * and creates a newValue variable within the block’s scope.
+ * The didSet property observer is triggered right **after** the property is changed
+ * and creates an oldValue within the block’s scope.
+ */
+struct Employee {
+    var hourlyWage = 15 {
+        willSet {
+            print("The hourly wage is about to be changed from \(hourlyWage) to \(newValue)")
+        }
+        didSet {
+            print("The hourly wage has been changed from \(oldValue) to \(hourlyWage)")
+        }
+    }
+}
+
+// Prints:
+// The hourly wage is about to be changed from 15 to 20
+// The hourly wage has been changed from 15 to 20
+var codey = Employee()
+codey.hourlyWage = 20
+
+//: ## Private Setters
+/**
+ * Properties marked as private(set) can be accessed from outside the scope of its structure,
+ * but only assigned within it. This allows the setter to be more restrictive than the getter.
+ */
+struct User2 {
+    private(set) var name: String
+    mutating func updateName(to newName: String) {
+        if newName != "" {
+            name = newName
+        }
+    }
+}
+
+var currentUser = User2(name: "codey")
+currentUser.updateName(to: "Codey")
+print(currentUser.name)
+// This line doesn't compile because the 'name' setter is inaccessible
+// currentUser.name = "Bob"
+
+
+//: ## Extensions
+/**
+ *  The extension keyword is used to continue defining an existing class, structure, or enumeration from anywhere in a codebase.
+ *  Extensions can have new methods, internal types, and computed properties,
+ *  but can’t contain new stored properties.
+ */
+
---
+++ DongguoPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,79 @@
+//: [Previous](@previous)
+
+import Foundation
+//: # Closures
+/**
+ * Closures are self-contained blocks of functionality.
+ * Just like functions, closures take in arguments, execute instructions, and return a value or Void.
+ */
+
+//: ## Closures cannot have argument labels, only internal argument names
+
+//: ## Closures can be passed as arguments into functions.
+func combine(_ a: Int, _ b: Int, using combiner: (Int, Int) -> Int) -> Int {
+  return combiner(a,b)
+}
+
+let add = { (a: Int, b: Int) -> Int in
+  return a + b
+}
+
+let multiply = { (a: Int, b: Int) -> Int in
+  return a * b
+}
+
+print(combine(2,5, using: add)) // Prints: 7
+print(combine(2,5, using: multiply)) // Prints: 10
+
+//: ## Closures can be passed as arguments into functions.
+let sum = combine(2,5) { (a: Int, b: Int) -> Int in
+  return a + b
+}
+print(sum) // Prints: 7
+
+//: ## the arguments in parentheses, return type, and the keyword in can be omitted in exchange for shorthand argument labels
+let multipler = combine(3, 3) {$0 * $1}
+print(multipler)
+
+//: ## Common Higher-order Functions
+//: **filter** **map** **reduce** **sorted
+/**
+ * A higher-order function is a function that takes another function as an argument
+ */
+
+//: ## Escaping Closures
+//: A closure escapes a function when it’s called after the function returns.
+//: This can happen when the closure is assigned to a variable.
+//: Escaping closures must be marked with the @escaping tag in a function signature.
+struct TextSaver {
+  var saveAction: (String) -> Void = { print("Saving '\($0)' to disk") }
+
+  mutating func setSaveAction(to newAction: @escaping (String) -> Void) {
+    saveAction = newAction
+  }
+}
+
+var saver = TextSaver()
+saver.saveAction("Hello World!")
+// Prints: Saving 'Hello World!' to disk
+saver.setSaveAction(to: { print("Saving '\($0)' to the cloud") })
+saver.saveAction("Hello World!")
+// Prints: Saving 'Hello World!' to the cloud
+
+//: ## Capturing Values
+//: A closure can capture constants and variables
+//: from the surrounding context in which it’s defined.
+//: When a closure captures a value, it keeps track of it and can manipulate the value even if the original function returns.
+
+
+//: In Swift, the simplest form of a closure that can capture values is a nested function, written within the body of another function. A nested function can capture any of its outer function’s arguments and can also capture any constants and variables defined within the outer function.
+func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+    var runningTotal = 0
+    func incrementer() -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    // incrementer is returned by makeIncrementer as a closure
+    return incrementer
+}
+//: [Next](@next)
