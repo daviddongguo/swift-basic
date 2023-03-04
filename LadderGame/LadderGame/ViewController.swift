899f7871bd31bb7a002f0c729c7e331a005ee291
dongguowu <map07@BH408-05.local>
2023-03-04 09:46:41 -0500

    init new project

---
+++ LadderGame/LadderGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LadderGame
+//
+//  Created by map07 on 2023-03-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
