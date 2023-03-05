2c0c56c534944d28cc6d46c8a44abe1743dc797b
dongguowu <david.dong.guo@gmail.com>
2023-03-05 00:56:28 -0500

    can move king

--- LadderGame/LadderGame/ViewController.swift
+++ LadderGame/LadderGame/ViewController.swift
@@ -11,59 +11,78 @@ class ViewController: UIViewController {
     
     @IBOutlet var holder: UIView!
     
-
+    
+    @IBOutlet weak var goButton: UIButton!
+    
+    @IBOutlet weak var diceView: UIView!
+    
+    let diceArray = [UIImage(named: "number01"), UIImage(named: "number02")]
+    var player: UIButton?
+    var cellSize: Double = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cellSize = holder.frame.size.width / 10
+        player = createPlayer(n: 18, cellSize: cellSize)
+        
     }
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         setupBoardPad()
+        
     }
     
     private func setupBoardPad() {
-        let cellSize: CGFloat = holder.frame.size.width / 10
-
-        var player = createPlayer(n: 18, cellSize: cellSize)
-        holder.addSubview(player)
         
-        for n in 1...80 {
-            moveTo(player, n: n, cellSize: cellSize)
-        }
+
+        holder.addSubview(player!)
+        
+        goButton.addTarget(self, action: #selector(goButtonPressed),  for: .touchUpInside)
+        
+        
         
     }
     
-    private func moveTo(_ button: UIButton, n: Int, cellSize: Double) {
-        var x = ((n - 1) / 10 % 2 == 0) ? (n - 1) % 10 : 9 - (n - 1) % 10
-        var y = 9 - (n - 1) / 10
-        button.frame = CGRectMake(cellSize * Double(x), cellSize * Double(y), cellSize, cellSize )
+    @objc func goButtonPressed(_ sender: UIButton) {
+        moveTo(player!, n: 21, cellSize: cellSize)
         
     }
+}
 
-    
-    private func createPlayer(n: Int, cellSize: Double) -> UIButton {
-        
-        var x = ((n - 1) / 10 % 2 == 0) ? (n - 1) % 10 : 9 - (n - 1) % 10
-        var y = 9 - (n - 1) / 10
-        let player = UIButton(
-            frame: CGRect(
-                x: cellSize * Double(x),
-                y: cellSize * Double(y),
-                width: cellSize,
-                height: cellSize))
-        player.setTitle("king", for: .normal)
-        player.setTitleColor(.black, for: .normal)
-        player.backgroundColor = .red
-        player.layer.borderWidth = 2
-        player.layer.borderColor = UIColor.black.cgColor
-        
-
-        return  player
-    }
+private func moveTo(_ button: UIButton, n: Int, cellSize: Double) {
+    var x = ((n - 1) / 10 % 2 == 0) ? (n - 1) % 10 : 9 - (n - 1) % 10
+    var y = 9 - (n - 1) / 10
+    button.frame = CGRectMake(cellSize * Double(x), cellSize * Double(y), cellSize, cellSize )
     
 }
+
+
+private func createPlayer(n: Int, cellSize: Double) -> UIButton {
+    
+    let image = UIImage(named: "king.png")
+    
+    var x = ((n - 1) / 10 % 2 == 0) ? (n - 1) % 10 : 9 - (n - 1) % 10
+    var y = 9 - (n - 1) / 10
+    let player = UIButton(
+        frame: CGRect(
+            x: cellSize * Double(x),
+            y: cellSize * Double(y),
+            width: cellSize,
+            height: cellSize))
+    //        player.setTitle("king", for: .normal)
+    player.setTitleColor(.black, for: .normal)
+    player.backgroundColor = .red
+    player.setBackgroundImage(image, for: UIControl.State.normal)
+    player.layer.borderWidth = 2
+    player.layer.borderColor = UIColor.black.cgColor
+    
+    
+    return  player
+}
+
 
