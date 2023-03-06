48778aa98c5cf7029c22b98e2750c1d58786571b
dongguowu <david.dong.guo@gmail.com>
2023-03-06 09:27:20 -0500

    use character array

--- DongguoPlayground.playground/Pages/character.xcplaygroundpage/Contents.swift
+++ DongguoPlayground.playground/Pages/character.xcplaygroundpage/Contents.swift
@@ -1,33 +1,30 @@
-//: [Previous](@previous)
-
 import Foundation
-let lineBreaks = """
 
-This string starts with a line break.
-It also ends with a line break.
+// character array
+let str = "abcdefghijklmnopqrstuvwxyz"
+let characterArray = Array(str)
+print(characterArray)
 
-"""
-//print(lineBreaks)
-let oneCoolDude = "\u{1F60E}"
-let myChar = "\u{0061}"
-let acute = "\u{0301}"
-oneCoolDude
-//print(oneCoolDude)
+let aScalars = "a".unicodeScalars
+let aCode = aScalars[aScalars.startIndex].value
+let letters: [Character] = (0..<26).map {
+    i in Character(UnicodeScalar(aCode + i)!)
+}
 
+print(letters == characterArray)
 
 var playground = "playground"
-//print("Number of characters in \"\(playground)\": \(playground.count)")
-//print("Unicode scalar of each character in a string:")
-//for scalar in playground.unicodeScalars {
-//        print(" \(scalar) - \(scalar.value) ")
-//}
+print("Number of characters in \"\(playground)\": \(playground.count)")
+print("Unicode scalar of each character in a string:")
+for scalar in playground.unicodeScalars {
+        print(" \(scalar) - \(scalar.value) ")
+}
 
 /**
  * A Unicode scalar representes a single character in the string. But they are not the hexadecimal Unicode numbers. Instead, each is represented as an unsigned 32-bit integer.
  
  * aAcute was created using two Unicode scalars, and aAcutePrecomposed only used one. Why does Swift say that they are equivalent? The answer is canonical equivalence.
  */
-
 let aAcutePrecomposed = "\u{00E1}"
 let aAcute = "\u{0061}\u{0301}"
 let b = (aAcute == aAcutePrecomposed)
@@ -75,16 +72,17 @@ playground.hasPrefix("bc")
 playground.contains("cde")
 playground.contains("de")
 
+// With Swift 5, you can use the following Playground sample code in order to get an array of characters from a range of Unicode scalars
+// 1.
+let unicodeScalarRange: ClosedRange<Unicode.Scalar> = "A" ... "Z"
+// 2.
+let unicodeScalarValueRange: ClosedRange<UInt32> = unicodeScalarRange.lowerBound.value ... unicodeScalarRange.upperBound.value
+// 3.
+let unicodeScalarArray: [Unicode.Scalar] = unicodeScalarValueRange.compactMap(Unicode.Scalar.init)
+// 4.
+let characterArray2: [Character] = unicodeScalarArray.map(Character.init)
 
-let badStart = """
-    one
-    two
-    
-    """
-let end = """
-    three
-    """
-//print(badStart + end)
-
-
-
+print(characterArray2)
+/*
+ prints: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+ */
