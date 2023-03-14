3968710840b58158be850dfd4a7cf57c568e982e
dongguowu <david.dong.guo@gmail.com>
2023-03-14 13:19:02 -0400

    layout

--- TableView/TableView/TableViewController.swift
+++ TableView/TableView/TableViewController.swift
@@ -11,20 +11,20 @@ class TableViewController: UITableViewController {
     
     
     // Table view data source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return emojis.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiTableViewCell", for: indexPath)
-        
-        let emoji = emojis[indexPath.row]
-        
-        cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
-        cell.detailTextLabel?.text = emoji.description
-        
-        return cell
-    }
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return emojis.count
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiTableViewCell", for: indexPath)
+//        
+//        let emoji = emojis[indexPath.row]
+//        
+//        cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
+//        cell.detailTextLabel?.text = emoji.description
+//        
+//        return cell
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
