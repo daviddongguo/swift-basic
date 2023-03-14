63abdfbad5a16d4f25efeaa66f9d9f3067e58c78
dongguowu <david.dong.guo@gmail.com>
2023-03-14 12:35:47 -0400

    new Table view project

---
+++ TableView/TableView/Emoji.swift
@@ -0,0 +1,22 @@
+//
+//  Emoji.swift
+//  TableView
+//
+//  Created by map07 on 2023-03-14.
+//
+
+import Foundation
+
+class Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    init(symbol: String, name: String, description: String, usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
