bede1b8e8b29746823d583d57b8c8fb169c7645f
dongguowu <david.dong.guo@gmail.com>
2023-03-19 14:55:34 -0400

    finish back-end

--- LeetCode/LeetCode/main.swift
+++ LeetCode/LeetCode/main.swift
@@ -1,13 +1,8 @@
-//
-//  main.swift
-//  LeetCode
-//
-//  Created by map07 on 2023-03-02.
-//
+
 
 import Foundation
 
-let operandMax = 20
+let operandMax = 1
 
 enum OperationEnum: String {
     case addition = "+"
@@ -29,57 +24,65 @@ enum OperationEnum: String {
     }
 }
 
-struct Quiz {
+class Quiz: CustomStringConvertible {
     let id: Int
     let title: String
+    func IsRightAnser(_ userAnswer: String) -> Bool {
+        return false
+    }
+    init(id: Int, title: String) {
+        self.id = id
+        self.title = title
+    }
+    
+    var description: String {
+        return self.title
+    }
 }
 
-struct MathQuiz : CustomStringConvertible{
-    let leftOperand: Double
-    let rightOperand: Double
+class MathQuiz : Quiz{
+    let leftOperand: Int
+    let rightOperand: Int
     let operation: OperationEnum
     let answer: Double
-    var userInput: String? = nil
-    func IsRightAnswer(_ userAnser: String) -> Bool {
-        guard let userAnser = Double(userAnser) else {
+    var userAnswer: String? = nil
+    func IsRightAnswer(_ userAnswer: String) -> Bool {
+        guard let userAnser = Double(userAnswer) else {
             return false
         }
         return (userAnser - self.answer) > -0.01 &&  (userAnser - self.answer) < 0.01
     }
     
-    private static let operations: [OperationEnum] = [.addition, .subtraction, .multiplication, .division]
     
-    init() {
-        self.operation = MathQuiz.operations.randomElement()!
-        if(self.operation == OperationEnum.division){
-            self.rightOperand = Double(Int.random(in: 1...operandMax))
-        }else {
-            self.rightOperand = Double(Int.random(in: 0...operandMax))
-            
-        }
-        self.leftOperand = Double(Int.random(in: 0...operandMax))
-        
-        switch self.operation {
-        case .addition:
-            self.answer = self.leftOperand + self.rightOperand
-        case .subtraction:
-            self.answer = self.leftOperand - self.rightOperand
-        case .multiplication:
-            self.answer = self.leftOperand * self.rightOperand
-        case .division:
-            self.answer = self.leftOperand / self.rightOperand
-        }
+    init(id: Int, leftOperand: Int, rightOperand: Int, operation: OperationEnum) {
+        self.leftOperand = leftOperand
+        self.rightOperand = rightOperand
+        self.operation = operation
+        self.answer = self.operation.apply(Double(self.leftOperand), Double(self.rightOperand))
+        super.init(id: id, title: String(format: "%d %@ %@", self.leftOperand, self.operation.rawValue, self.rightOperand < 0 && self.operation == .subtraction ? "(\(self.rightOperand))" : "\(self.rightOperand)"))
     }
-  
-    var description: String {
-        return String(format: "%.0f \(self.operation.rawValue) %.0f = %.2F", self.leftOperand,  self.rightOperand, self.answer)
-    }
-    
 }
 
-for _ in 1...5 {
-    let quiz = MathQuiz()
+class RandomMathQuiz : MathQuiz{
+    private static let operations: [OperationEnum] = [.addition, .subtraction, .multiplication, .division]
+    
+    init(id: Int) {
+        let leftOperand = Int.random(in: -operandMax...operandMax)
+        let operation = RandomMathQuiz.operations.randomElement()!
+        let intArray = (-operandMax...operandMax).filter { operation != .division || $0 != 0 }
+        let rightOperand = intArray.randomElement()!
+        
+        super.init(id: id, leftOperand: leftOperand,  rightOperand: rightOperand,  operation: operation)
+    }
+    
+    override var description: String {
+        return super.description + " =  \(self.answer)"
+    }
+  
+}
+
+for i in 1...50 {
+    let quiz = RandomMathQuiz(id: i)
     print(quiz.description)
-    print(quiz.IsRightAnswer("0.0"))
 }
 
