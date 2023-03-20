4653c3849133a7863f85d9c83794ceb5c3081412
dongguowu <david.dong.guo@gmail.com>
2023-03-20 16:09:15 -0400

    add comparable

--- Habits/Habits/HabitCollectionViewController.swift
+++ Habits/Habits/HabitCollectionViewController.swift
@@ -14,16 +14,29 @@ class HabitCollectionViewController: UIViewController {
     deinit { habitsRequestTask?.cancel() }
     
     enum ViewModel {
-        enum Section: Hashable{
+        enum Section: Hashable, Comparable {
+            static func < (lhs: HabitCollectionViewController.ViewModel.Section, rhs: HabitCollectionViewController.ViewModel.Section) -> Bool {
+                switch(lhs, rhs) {
+                case (.category(let l), .category(let r)):
+                    return l.name < r.name
+                case (.favorites, _):
+                    return true
+                case (_, favorites):
+                    return false
+                }
+            }
+            
             case favorites
             case category(_ category: Category)
         }
-        
-        typealias Item = Habit
+            typealias Item = Habit
     }
     
     struct Model {
         var habitsByName = [String: Habit]()
+        var favoriteHabits: [Habit] {
+            return Settings.shared.favoriteHabits
+        }
     }
     
     var dataSource: DataSourceType!
@@ -51,6 +64,21 @@ class HabitCollectionViewController: UIViewController {
     }
     
     func updateCollectionView() {
+        let itemsBySection = model.habitsByName.values.reduce(into: [ViewModel.Section: [ViewModel.Item]]()) { partial, habit in
+            let item = habit
+            let section: ViewModel.Section
+            if model.favoriteHabits.contains(habit) {
+                section = .favorites
+            } else {
+                section = .category(habit.category)
+            }
+            
+            partial[section, default: []].append(item)
+        }
+        
+        let sectionIDs = itemsBySection.keys.sorted()
+        
+        
         
     }
     
