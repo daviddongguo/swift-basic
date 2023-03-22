9dfcd36ce53e7a031835b3d526229dde4358eca0
dongguowu <david.dong.guo@gmail.com>
2023-03-22 09:43:22 -0400

    new final test project

---
+++ MovieProject/dongguo/dongguo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dongguo
+//
+//  Created by david on 2023-03-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
